3af80b7ccb7a1a046914212e34aef112d6bf7d6b
Daniel Jimenez Pacheco <daninafria@hotmail.es>
2020-02-01 19:21:09 +0100

    Anuncio intersticial
    Implementación aunucio instersticial

--- BiruBiruPrueba/app/src/main/java/com/chronoout/birubiru/drinking/games/game/cards/casual/friends/party/drink/pruebaanuncios/MainActivity.java
+++ BiruBiruPrueba/app/src/main/java/com/chronoout/birubiru/drinking/games/game/cards/casual/friends/party/drink/pruebaanuncios/MainActivity.java
@@ -2,15 +2,21 @@ package com.chronoout.birubiru.drinking.games.game.cards.casual.friends.party.dr
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private AdView mAdView;
+    private InterstitialAd mInterstitialAd;
+    private Button myButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +30,57 @@ public class MainActivity extends AppCompatActivity {
         mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        myButton = findViewById(R.id.button);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mInterstitialAd.isLoaded()){
+                    mInterstitialAd.show();
+                }else {
+                    System.out.println(" anuncio no disponibles aun");
+                }
+            }
+        });
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                //mInterstitialAd.show();
+                System.out.println(" anuncio cargado");
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when the ad is displayed.
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the interstitial ad is closed.
+            }
+        });
+
+
+
     }
 }
