79e4e9b5807d06c3708a557c8affffb4d0d079fa
Daniel Jimenez Pacheco <daninafria@hotmail.es>
2020-02-01 18:36:41 +0100

    Creacion Proyecto
    ProyectoCreado

---
+++ BiruBiru/app/src/main/java/com/chronoout/drinking/games/game/cards/casual/friends/party/drink/alcohol/neverhaveiever/birubiru/MainActivity.java
@@ -0,0 +1,14 @@
+package com.chronoout.drinking.games.game.cards.casual.friends.party.drink.alcohol.neverhaveiever.birubiru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ BiruBiru/app/src/test/java/com/chronoout/drinking/games/game/cards/casual/friends/party/drink/alcohol/neverhaveiever/birubiru/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.chronoout.drinking.games.game.cards.casual.friends.party.drink.alcohol.neverhaveiever.birubiru;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
